fc6418bf5bfa3f1e27853797e3f20e537496b47b
Michal Stepien <michal.stepien36@gmail.com>
2019-07-22 23:20:19 +0200

    pop up menu

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const menuFooter = document.querySelector("footer");
 const menuSocial = document.querySelector(".menu-social");
-const navItems = document.querySelectorAll(".menu-nav__item");
+const navItems = document.querySelectorAll(".menu__item");
 
 // Set Initial State Of Menu
 let showMenu = false;
@@ -16,22 +16,22 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     // menu.classList.add("show");
-    // menuNav.classList.add("show");
-    // menuBranding.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
     // menuFooter.classList.add("show");
     // menuSocial.classList.add("show");
-    // navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach(item => item.classList.add("show"));
 
     // Set Menu State
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     // menu.classList.remove("show");
-    // menuNav.classList.remove("show");
-    // menuBranding.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
     // menuFooter.classList.remove("show");
     // menuSocial.classList.remove("show");
-    // navItems.forEach(item => item.classList.remove("show"));
+    navItems.forEach(item => item.classList.remove("show"));
 
     // Set Menu State
     showMenu = false;
