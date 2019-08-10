812ac107ec92843044ff93c357d8b64c7eb73a80
Michal Stepien <michal.stepien36@gmail.com>
2019-08-10 20:27:53 +0200

    vector logo animation

--- js/main.js
+++ js/main.js
@@ -39,6 +39,11 @@ function closeMenu() {
   showMenu = false;
 }
 
+const logo = document.querySelectorAll("#logo path");
+for (let i = 0; i < logo.length; i++) {
+  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
+}
+
 //skills script
 
 // const icon = document.querySelector(".icon");
