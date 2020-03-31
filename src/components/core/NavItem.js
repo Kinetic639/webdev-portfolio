8987343e8878dc2abe1ee727457efdc0c7d32b0f
Michal Stepien <michal.stepien36@gmail.com>
2020-03-31 08:19:28 +0200

    pages

--- src/components/core/NavItem.js
+++ src/components/core/NavItem.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import {  NavLink } from "react-router-dom";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavItem(props) {
-    const isActive = {
-        color: '#9acd32'
-    }
-    return (
-    <li className='nav-menu__item'>
-<NavLink activeStyle={isActive} to={props.page} onClick={props.click}>{props.children}</NavLink>
+  // const isActive = {
+  //     color: '#9acd32'
+  // }
+  return (
+    <li className="nav-menu__item">
+      <NavLink
+        exact
+        activeClassName={"is-active"}
+        to={props.page}
+        onClick={props.click}
+      >
+        {props.children}
+      </NavLink>
     </li>
-    )
+  );
 }
