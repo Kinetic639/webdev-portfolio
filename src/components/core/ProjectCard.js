aae91630ee385c463c563fe1699486c52b2c091b
Kinetic639 <michal.stepien36@gmail.com>
2020-06-30 00:21:39 +0200

    projectsPage state component, passing props to project card

--- src/components/core/ProjectCard.js
+++ src/components/core/ProjectCard.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-const ProjectCard = () => {
+const ProjectCard = ({ id, title, description }) => {
   return (
     <div className="project-card">
-      <div className="project-card__number-container"> <h1 className="project-card__number">01</h1></div>
+      <div className="project-card__number-container">
+        {" "}
+        <h1 className="project-card__number">{id}</h1>
+      </div>
       <div className="project-card__img"></div>
       <div className="project-card__description">
-        <h2 className="project-card__description-title">Budget Menager</h2>
-        <p className="project-card__description-text">
-          My biggest project so far. React and Redux on the front end and
-          firebase database on the backend. 
-        </p>
+        <h2 className="project-card__description-title">{title}</h2>
+        <p className="project-card__description-text">{description}</p>
         <button className="btn btn__project-card">info...</button>
       </div>
     </div>
--- src/components/pages/ProjectsPage.js
+++ src/components/pages/ProjectsPage.js
@@ -1,20 +1,80 @@
-import React from "react";
 import ProjectCard from "../core/ProjectCard";
 import ContactPage from "./ContactPage";
 
-const ProjectsPage = () => {
-  return (
-    <div className="page page-projects">
+import React, { Component } from 'react'
+
+export class ProjectsPage extends Component {
+  state = {
+    projects: [
+      {
+        id: '01',
+        title: "Expenses Menager",
+        description:
+          "My biggest project so far. React and Redux on the front end and firebase database on the backend. ",
+        imgUrl: "./images/projects/project1.jpg",
+        stack: [
+          "Html 5",
+          "Sass(SCSS)",
+          "Javascript",
+          "React",
+          "Redux",
+          "Jest",
+          "Enzyme",
+
+          "Firebase",
+          "Heroku",
+        ],
+        features: [
+          "Build with React",
+          "Google Authentication",
+          "Firebase realtime database",
+        ],
+        github: "https://github.com/Kinetic639/budget-app",
+        live: "https://simple-budget-menager.herokuapp.com/",
+      },
+      {
+        id: '02',
+        title: "Portfolio website",
+        description: "It's the website you are currently visiting",
+        imgUrl: "./images/projects/project2.jpg",
+        stack: [
+          "Html 5",
+          "Javascript",
+          "React",
+          "Sass(SCSS)",
+          "Heroku",
+          "Figma",
+        ],
+        features: [],
+        github: "https://github.com/Kinetic639/webdev-portfolio",
+        live: "http://www.michal-stepien.eu/",
+      },
+      {
+        id: '03',
+        title: "Random value picker",
+        description:
+          "Simple app, where you create list of values and let the computer pick one of them at random. ",
+        imgUrl: "./images/projects/project3.jpg",
+        stack: ["Html 5", "React", "Javascript", "Sass(SCSS)", "Heroku"],
+        features: ["Forms", "Using local storage"],
+        github: "https://github.com/Kinetic639/indecision",
+        live: "https://random-value-picker.herokuapp.com/",
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <div className="page page-projects">
       {/* <Header className="header-projects">My Projects</Header> */}
       <div className="content-container container-projects">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {this.state.projects.map(project => <ProjectCard {...project}/>)}
         <ContactPage />
       </div>
     </div>
-  );
-};
+    )
+  }
+}
 
-export default ProjectsPage;
+export default ProjectsPage
+
