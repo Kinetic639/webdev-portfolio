895f6fe19b348cee72bd52c13d91da893baa491b
Kinetic639 <michal.stepien36@gmail.com>
2020-06-29 12:36:55 +0200

    contact page RWD

--- src/components/pages/ContactPage.js
+++ src/components/pages/ContactPage.js
@@ -17,16 +17,16 @@ export default function ContactPage() {
       <div className="content-container container-contact">
         {createBrowserHistory().location.pathname !== "/contact" ? (
           <div className="questions">
-            <h3>
-              Contact me
-            </h3>
+            <h3>Contact me</h3>
           </div>
         ) : null}
         <div className="location">
-          <h3>
-            Location: <span>Poznań, Poland</span>
-          </h3>
-          <p>(I don't mind relocating if necessary)</p>
+          <h1 className="page__title--contact">
+            <span>Location:</span> Poznań, Poland
+          </h1>
+          <h2 className="page__subtitle--contact">
+            (I don't mind relocating if necessary)
+          </h2>
         </div>
         <div className="container-social">
           <a
